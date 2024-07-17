4de366b593a56eda706cfb84fdfbca82145c43fe
Nitin Pal <141559894+nitinpal90@users.noreply.github.com>
2024-07-17 22:58:56 +0530

    Create 197. Rising Temperature.sql

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,23 @@
+Table: Weather
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+-- Solution --
+
+SELECT W1.id
+FROM Weather W1
+JOIN Weather W2
+ON W1.recordDate = DATE_ADD(W2.recordDate, INTERVAL 1 DAY)
+WHERE W1.temperature > W2.temperature;
